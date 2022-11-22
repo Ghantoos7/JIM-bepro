009483d1fb22f23066200502ae49ecdd1265eafa
Georgio <ghnatios1@gmail.com>
2022-11-22 14:28:29 +0200

    right side of hero

--- src/componets/Hero/Hero.jsx
+++ src/componets/Hero/Hero.jsx
@@ -1,32 +1,68 @@
 import React from 'react'
 import './Hero.css'
 import Header from '../Header/Header'
-import '../../App.css'
+import hero_image from '../../assets/hero_image.png'
+import hero_image_back from '../../assets/hero_image2.png'
+import heart from '../../assets/heart.png'
+import calories from '../../assets/calories.png'
 
 const Hero = () => {
   return (
     <div className="hero">
-        <div className="left-h">
-          <Header/>
-          <div className="the-best-ad">
-            <div></div>
-            <span>the best fitness club in the town</span>
-          </div>
 
+      <div className="left-h">
+        <Header/>
 
-          <div className="hero-text">
-            <div>
-              <span className='stroke-text'>Shape</span>
-              <span> Your</span>
-            </div>
-            <div>
-              <span>Ideal body</span>
-              </div>
-              <div><span>In here we will help you to shape and build your ideal body and live up your life to fullest</span></div>
-          </div>
+        <div className="the-best-ad">
+          <div></div>
+          <span>the best fitness club in the town</span>
+        </div>
 
+        <div className="hero-text">
+          <div>
+            <span className='stroke-text'>Shape</span>
+            <span> Your</span>
           </div>
-        <div className="right-h">right side</div>
+          <div>
+            <span>Ideal body</span>
+          </div>
+          <div><span>In here we will help you to shape and build your ideal body and
+            live up your life to fullest</span></div>
+        </div>
+
+        <div className="figures">
+          <div>
+            <span>2+</span>
+            <span>Expert coaches</span>
+          </div>
+          <div>
+            <span>978</span>
+            <span>Members joined</span>
+          </div>
+          <div>
+            <span>50+</span>
+            <span>fitness Programs</span>
+          </div>
+        </div>
+
+        <div className="hero-buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn more</button>
+        </div>
+      </div>
+
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+
+        <div className="heart-rate">
+          <img src={heart} alt="" />
+          <span>Heart Rate</span>
+          <span>123 bpm</span>
+        </div>
+      <img src={hero_image} alt="" className="hero-image" />
+      <img src={hero_image_back} alt="" className="hero-image-back" />
+      </div>
+
     </div>
   )
 }
