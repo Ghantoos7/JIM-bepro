028e31389ba6e5ecc021fa3664f32fc860059e71
Georgio <ghnatios1@gmail.com>
2022-11-22 12:25:55 +0200

    header and figures

--- src/componets/Hero/Hero.jsx
+++ src/componets/Hero/Hero.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './Hero.css'
-export const Hero = () => {
+import Header from '../Header/Header'
+import '../../App.css'
+
+const Hero = () => {
   return (
     <div className="hero">
-        <div className="left-h">left side</div>
+        <div className="left-h">
+          <Header/>
+          <div className="the-best-ad">
+            <div></div>
+            <span>the best fitness club in the town</span>
+          </div>
+
+
+          <div className="hero-text">
+            <div>
+              <span className='stroke-text'>Shape</span>
+              <span> Your</span>
+            </div>
+            <div>
+              <span>Ideal body</span>
+              </div>
+              <div><span>In here we will help you to shape and build your ideal body and live up your life to fullest</span></div>
+          </div>
+
+          </div>
         <div className="right-h">right side</div>
     </div>
   )
