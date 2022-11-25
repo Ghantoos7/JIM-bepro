35fdddce781696d04226b529c5c93826d5edaf0c
Georgio <ghnatios1@gmail.com>
2022-11-25 16:32:22 +0200

    graph

--- src/componets/Hero/Hero.jsx
+++ src/componets/Hero/Hero.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import './Hero.css'
-import Header from '../Header/Header'
-import hero_image from '../../assets/hero_image.png'
-import hero_image_back from '../../assets/hero_image2.png'
-import heart from '../../assets/heart.png'
-import calories from '../../assets/calories.png'
+import React from "react";
+import "./Hero.css";
+import Header from "../Header/Header";
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image2.png";
+import heart from "../../assets/heart-beat.gif";
+import calories from "../../assets/graph.gif";
 
 const Hero = () => {
   return (
     <div className="hero">
-
       <div className="left-h">
-        <Header/>
+        <Header />
 
         <div className="the-best-ad">
           <div></div>
@@ -20,14 +19,18 @@ const Hero = () => {
 
         <div className="hero-text">
           <div>
-            <span className='stroke-text'>Shape</span>
+            <span className="stroke-text">Shape</span>
             <span> Your</span>
           </div>
           <div>
             <span>Ideal body</span>
           </div>
-          <div><span>In here we will help you to shape and build your ideal body and
-            live up your life to fullest</span></div>
+          <div>
+            <span>
+              In here we will help you to shape and build your ideal body and
+              live up your life to fullest
+            </span>
+          </div>
         </div>
 
         <div className="figures">
@@ -59,12 +62,19 @@ const Hero = () => {
           <span>Heart Rate</span>
           <span>123 bpm</span>
         </div>
-      <img src={hero_image} alt="" className="hero-image" />
-      <img src={hero_image_back} alt="" className="hero-image-back" />
+        <img src={hero_image} alt="" className="hero-image" />
+        <img src={hero_image_back} alt="" className="hero-image-back" />
+
+        <div className="calories">
+          <img src={calories} alt="calories" />
+          <div>
+            <span>Calories Burned</span>
+            <span>220 kcal</span>
+          </div>
+        </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
