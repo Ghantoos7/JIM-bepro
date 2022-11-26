0e96a7d0c88d5d805895f1b1c36b45ff7914d934
Georgio <ghnatios1@gmail.com>
2022-11-26 23:02:16 +0200

    reasons final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hero from './componets/Hero/Hero';
 import Programs from './componets/Programs/Programs';
+import Reasons from './componets/Reasons/Reasons'
 function App() {
   return (
     <div className="App">
          <Hero/>
          <Programs/>
+         <Reasons/>
     </div>
   );
 }
